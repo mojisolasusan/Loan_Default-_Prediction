e494f155366120f1b10616c7fccdfc9e2a0f6a14
Mojisola_susan <147939844+mojisolasusan@users.noreply.github.com>
2024-03-08 22:46:13 +0000

    Update loan.py

--- loan.py
+++ loan.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import joblib
 from joblib import load 
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA 
